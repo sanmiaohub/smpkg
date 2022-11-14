df3e039d299cc69dc82d848e5bf6382338677b8a
梅杰 <121302155@qq.com>
2022-11-14 17:15:56 +0800

    修改数据库返回结构

--- database.go
+++ database.go
@@ -6,13 +6,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func GetDBInst(user string, password string, host string, port string, dbname string) *sql.DB {
+func GetDBInst(user string, password string, host string, port string, dbname string) (dbInst *sql.DB, err error) {
 	dbFormat := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s", user, password, host, port, dbname, "utf8")
-
-	var err error
-	dbInst, err := sql.Open("mysql", dbFormat)
-	if err != nil {
-		panic(any("数据库链接异常:" + err.Error()))
-	}
-	return dbInst
+	return sql.Open("mysql", dbFormat)
 }
