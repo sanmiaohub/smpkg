2cf09924e32f839a0dde18f5fb06da74088f2e1c
梅杰 <121302155@qq.com>
2022-11-30 17:45:48 +0800

    添加测试代码

---
+++ queue.go
@@ -0,0 +1,25 @@
+package smpkg
+
+func GetQueueKernel() *Kernel {
+	return &Kernel{}
+}
+
+type Kernel struct {
+	list map[string]Worker
+}
+
+func (k *Kernel) Register(queueName string, consumer Worker) {
+	k.list[queueName] = consumer
+}
+
+func (k *Kernel) Start() {
+	done := make(chan int, 1)
+	for qn, c := range k.list {
+		go c.Do(qn)
+	}
+	<-done
+}
+
+type Worker struct {
+	Do func(queue string)
+}
